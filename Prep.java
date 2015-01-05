4df6bff2faba185303583fa90e43557083e07e38
kaytrean <kaytrean@gmail.com>
2015-01-05 08:29:45 -0500

    Create Prep.java

---
+++ Prep.java
@@ -0,0 +1,10 @@
+  public class Prep extends Boys{
+  
+    public Prep(int approval, int time){
+      super (approval, time){ 
+    }
+    public String convo(You other, String userInput){
+    if (userInput.equals("1"){
+      return ("Hmm I see");}
+    }
+    }

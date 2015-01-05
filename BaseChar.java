4e98a20261560e361394b83bd49eb46635ee1439
Liiiian <ltaylor.wong@gmail.com>
2015-01-05 08:06:33 -0500

    Create BaseChar.java

---
+++ BaseChar.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.util.scanner.*;
+
+public class Char{
+    
+    public int energy=100;
+    public int 
+
+    public Char(String n){
+	setName(n);
+	setEnergy(energy);
+    }
+    public void setName(String name){
+	this.name=name;
+    }
+    public void setEnergy(int e){
+	this.energy=e;
+    }
+	

3d114d343534093c7feb328e04897960c7aadad3
Liiiian <ltaylor.wong@gmail.com>
2015-01-05 15:35:58 -0500

    Update Boys.java

--- Boys.java
+++ Boys.java
@@ -1,2 +1,14 @@
 public class Boys extends BaseChar{
+  public int approval;
+  public int timeSpent;
+  public boolean date;
+  
+  public boolean dateMe(){
+    if approval==100{
+      date=true;
+    }
+  }
+  
+  public 
 }
+

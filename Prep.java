4c70e240dc99d95a202b617f4a352ce57643a1c5
Liiiian <ltaylor.wong@gmail.com>
2015-01-06 08:27:23 -0500

    Update Prep.java

--- Prep.java
+++ Prep.java
@@ -4,7 +4,14 @@
       super (approval, time){ 
     }
     public String convo(You other, String userInput){
-    if (userInput.equals("1"){
-      return ("Hmm I see");}
+    if (userInput.equals("1")){
+      return ("Hmm I see");
+    }
+    if (userInput.equals("2")){
+      return("Ugh, can you go bother someone else?");
+      approval=approval-1;
+      annoyance=annoyance+5;
     }
     }
+    }
+  }

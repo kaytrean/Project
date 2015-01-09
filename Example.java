aa25e7e2cad7549e7cb3ed6f0fa84c59c67b5364
kaytrean <kaytrean@gmail.com>
2015-01-09 08:31:30 -0500

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+public class Example extends BaseChar {
+
+    Random thing = new Random();
+    
+    public Example(int h, int e, int a, int d, int s, int ac, int c, String n, String w){
+	 super(h,e,a,d,s,ac,c,n,w);
+    }
+    public String action(Monster other, String userInput){
+	if(userInput.equals( "1") && thing.nextInt(this.getACC()) > 20 ) {
+	    if ((this.getMP() - 20) < 0) {
+		return("Not enough energy!");
+	    } else {
+		int damage = 15 + thing.nextInt(11);
+		other.setHP(other.getHP() -  damage);
+		this.setMP(this.getMP() - 20);
+		return(h);
+	    }
+	}else if(userInput.equals("2") && thing.nextInt(this.getACC()) > 0) {
+	    if ((this.getMP() - 30) < 0) {
+		return("Not enough energy!");
+	    } else {
+	        int damage = 15 + thing.nextInt(11);
+		other.setHP(other.getHP() - damage);
+		this.setMP(this.getMP() - 30);
+		return(this + " talked to " + other + ". " + other + " is appalled and " + this + " has " + " energy.");
+	    }
+	}else if(userInput.equals("3") && thing.nextInt(this.getACC()) > 20) {
+	    if ((this.getMP() - 40) < 0) {
+		return("Not enough energy!");
+	    } else {
+		int damage = 25 + thing.nextInt(11);
+		other.setHP(other.getHP() - damage);
+	    this.setMP(this.getMP() - 40);
+	    return(this + " told a bad joke " + other + ". " + other + " lost " + damage + " approval");
+	    }
+	} else {
+	    if (userInput.equals("1")) {
+		this.setMP(this.getMP()-20);
+		return "You don't have enough energy";
+	    }
+	    if (userInput.equals("2")) {
+	        this.setMP(this.getMP()-30);
+		return "You don't have enough energy";
+	    }
+	    if (userInput.equals("3")) {
+		this.setMP(this.getMP()-40);
+		return "You don't have enough energy";
+	    }
+	    if (userInput.equals("")) {
+		return this.toString() + "stared into the distance...";
+	    }
+	    if (this.getMP() < 0) {
+		this.setMP(0); 
+	    }
+	    return "";
+	}
+    }	
+}

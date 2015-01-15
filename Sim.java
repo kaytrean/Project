ce5a39afd3cbf71d1eb5cd309c3f130611b3e6d6
kaytrean <kaytrean@gmail.com>
2015-01-15 08:41:55 -0500

    Update Sim.java

--- Sim.java
+++ Sim.java
@@ -2,6 +2,35 @@ public class Sim{
     public int day;
     public int energy=100;
 
+/*if (day >30){
+	endgame()
+}
+
+public void endgame(){
+	if (relationship = true){
+		do something
+	}
+	if (relationship = false){
+		shopkeeper()
+	}
+}
+talking =10
+working = 40
+dating = 50
+gift giving = 60
+
+items for shop:
+tea(goth)
+romance novel(gymnast)
+hoodie(gymnast)
+watch(prep)
+overpriced pencil(prep)
+dumbells(no one)
+cd(goth)
+flour(goth)
+mug(everyone)
+cologne(everyone)
+*/
 
     public void reEnergize(){
 	if (energy==0){

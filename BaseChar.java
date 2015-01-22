1cca1ed91e5c8c4d036f6e4eca515fd0ed216900
Liiiian <ltaylor.wong@gmail.com>
2015-01-22 08:39:31 -0500

    Update BaseChar.java

--- BaseChar.java
+++ BaseChar.java
@@ -6,11 +6,12 @@ import java.util.Random;
 public class BaseChar{
 
     public int energy=100;
-    public int strength=5;
-    public int brains=5;
-    public int charisma=5;
+    public int strength=10;
+    public int brains=10;
+    public int charisma=10;
     public boolean relationship=false;
     public int money=10;
+    public int day=1;
     public ArrayList<String> inventroy = new ArrayList<String>();
 
     public int getEnergy(){
@@ -28,6 +29,9 @@ public class BaseChar{
     public int getMoney(){
     	return money;
     }
+    public int getDays(){
+    	return day;
+    }
 
 
     public String learning(){
@@ -37,14 +41,17 @@ public class BaseChar{
 	if (n==3){
 	    afterLearning = "Hmm, this wasn't as hard as I expected... I really learned a lot today!";
 	    brains=brains+3;
+	    energy=energy-20;
 	    return afterLearning;
 	}else if (n==2){
 	    afterLearning = "Hey, I think I'm getting the hang of this!";
 	    brains=brains+2;
+	    energy=energy-20;
 	    return afterLearning;
 	}else{
 	    afterLearning = "Urgg! I don't understand any of this! I should just stop now...";
 	    brains=brains+1;
+	    energy=energy-20;
 	    return afterLearning;
 	}
     }
@@ -53,44 +60,59 @@ public class BaseChar{
     	String afterWorkout="";
     	Random f = new Random();
     	int x = f.nextInt(3)+1;
+    	if (energy>=20){
     	if (x==3){
     		afterWorkout = "Hmm, don't I feel healthy!";
     		strength = strength + 3;
+    		energy=energy-20;
     		return afterWorkout;
     	}else if (x==2){
     		afterWorkout = "Ewww, I feel all sweaty... at least I got some exercise.";
     		strength = strength + 2;
+    		energy=energy-20;
     		return afterWorkout;
     	}else{
     		afterWorkout = "I think that woman was staring at me...";
     		strength = strength + 1;
+    		energy=energy-20;
     		return afterWorkout;
 	    	}
+    }else{
+    	return "I think i've had enough for the day, I should sleep";
+    }
     }
     public String stroll(){
     	String afterStroll="";
     	Random w = new Random();
     	int z = w.nextInt(3)+1;
+    	if (energy>=20){
     	if (z==3){
     		afterStroll = "Isn't it just lovely today! I think I can hear the birds...";
     		charisma = charisma + 3;
+    		energy=energy-20;
     		return afterStroll;
     	}else if (z==2){
     		afterStroll = "What a nice walk! It looks a little cloudy though, I should leave.";
     		charisma = charisma + 2;
+    		energy=energy-20;
     		return afterStroll;
     	}else{
     		afterStroll = "I think the trees are speaking to me...";
     		charisma = charisma + 1;
+    		energy=energy-20;
     		return afterStroll;
 	    }
+	}else{
+		return "I think i've had enough for the day, I should sleep";
 	}
+    }
 	public String chores(){
     	String afterChores="";
     	Random c = new Random();
    	Random k = new Random();
    	int m=k.nextInt(10)+4;
     	int a = c.nextInt(3)+1;
+    	if (energy>=20){
     	if (a==3){
     		afterChores = "HOLLA HOLLA GET DOLLA!";
     		money = money + m;
@@ -104,7 +126,16 @@ public class BaseChar{
     		money = money + m;
     		return afterChores;
 	    }
+	}else{
+		return "I think i've had enough for the day, I should sleep";
 	}
+	}
+	public String sleep(){
+    	String afterSleep="";
+    	energy = 100;
+    	day++;
+    	afterSleep="I feel well rested! Let's get this day started";
+    	return afterSleep;
 }
 //more attractive the boy, the less affection gained
 //boolean relationship = false

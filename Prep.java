6a25be13a889b171ff5aef96298d2a3c0d1e223c
kaytrean <kaytrean@gmail.com>
2015-01-21 23:17:35 -0500

    Update Prep.java

--- Prep.java
+++ Prep.java
@@ -2,18 +2,26 @@
   
     public Prep(int approval, int time){
       super (approval, time){ 
-    }
-    public String convo(You other, String userInput){
-    if (userInput.equals("1")){
-      return ("Hmm I see");
-      annoyance=annoyance+1;
-    }
-    if (userInput.equals("2")){
-      return("Ugh, can you go bother someone else?");
-      approval=approval-1;
-      annoyance=annoyance+5;
-    }
-    }
-    }
-  }
+      }
+      public String convo(){
+      String afterConvo="";
+      Random r = new Random();
+      int n = r.nextInt(5)+1;
+      if (n == 5){
+       afterConvo = "I think you come here too often.";
+      }else if (n == 4){
+       afterConvo = "Hm? Studying again? You know you will never get to my level.";
+      }else if (n == 3){
+	  afterConvo = "If you are asking me to tutor you, the answer is no.";
+      }else if (n == 2){
+	  afterConvo = "Consider it an honor I am even talking to you.";
+      }else{
+	  afterConvo = "Hm? What do you want now?";
+      }
+      affection+= 1;
+      return afterConvo;
+      
+     }
+ }
+}
 //use Togami pictures
